0917f3be3845e828b2f13eeb50b106a597116874
Nuka0112 <67578977+Nuka0112@users.noreply.github.com>
2023-07-24 14:08:54 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+//const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
 const port = 3000
 
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
-
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
 
 app.get('/users', db.getUsers)
 app.get('/users/:id', db.getUserById)
@@ -21,14 +11,27 @@ app.post('/users', db.createUser)
 app.put('/users/:id', db.updateUser)
 app.delete('/users/:id', db.deleteUser)
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
 
 
 //Postman
 //http://localhost:3000/users/
 //{
 //  name: 'Gerich',
-//  hemail: 'test@mail.ru',
+//  email: 'test@mail.ru',
 //}
+
+/*
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+*/
